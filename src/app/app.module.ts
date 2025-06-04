c43c78eac205df7bc1b1f03ded7a5d1caa11d7ca
mehinp <mehinpandya@gmail.com>
2025-06-04 12:38:15 -0400

    some edits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -10,6 +11,7 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
 import { CartComponent } from './cart/cart.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SignInComponent } from './user/sign-in/sign-in.component';
+
 
 @NgModule({
   declarations: [
@@ -21,7 +23,7 @@ import { SignInComponent } from './user/sign-in/sign-in.component';
     CartComponent,
     SignInComponent,
   ],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/site-header/site-header.component.ts
+++ src/app/site-header/site-header.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IUser } from '_course-resources/user/user.model';
+import { UserService } from '_course-resources/user/user.service';
 
 @Component({
   selector: 'bot-site-header',
   templateUrl: './site-header.component.html',
   styleUrls: ['./site-header.component.css'],
 })
-export class SiteHeaderComponent {
-  constructor() {}
+export class SiteHeaderComponent implements OnInit {
+  user: IUser | null = null;
+  showSignOutMenu: boolean = false; 
+
+  constructor(private userService: UserService) {}
+  ngOnInit(){
+    this.userService.getUser().subscribe({
+    next: (user) => {this.user = user}
+  })
+  }
+
+  toggleSignOutMenu() {
+    this.showSignOutMenu = !this.showSignOutMenu;
+  }
+
+  signOut(){
+    this.userService.signOut();
+    this.showSignOutMenu = false; 
+  }
 }
